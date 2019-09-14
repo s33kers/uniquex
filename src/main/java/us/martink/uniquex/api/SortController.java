2a735c208ae0cc0cd24035fba33f2f9f46fa23d0
tadasmartinkus <Tadas.Martinkus@consult.luminorgroup.com>
2019-09-14 12:55:22 +0300

    download file of sorted students

--- src/main/java/us/martink/uniquex/api/SortController.java
+++ src/main/java/us/martink/uniquex/api/SortController.java
@@ -1,10 +1,15 @@
 package us.martink.uniquex.api;
 
 import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +18,7 @@ import us.martink.uniquex.api.dto.SortType;
 import us.martink.uniquex.api.dto.Student;
 import us.martink.uniquex.sort.SortStrategy;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +36,16 @@ public class SortController {
                                       @RequestParam MultipartFile studentsFile) throws IOException {
         MappingIterator<Student> studentsIter = CSV_MAPPER.readerWithSchemaFor(Student.class).readValues(studentsFile.getInputStream());
         return sortStrategy.get(type.getKey()).sortByMark(studentsIter.readAll());
+    }
+
+    //FIXME feels like not the right way to download file of sorted students
+    @PostMapping("/file")
+    public void downloadStudents(@RequestBody List<Student> students, HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=students.csv");
+        CsvSchema schema = CsvSchema.builder().addColumn("name").addColumn("mark").build();
+        ObjectWriter writer = CSV_MAPPER.writerFor(Student.class).with(schema);
+        writer.writeValues(response.getOutputStream()).writeAll(students);
     }
 
 }
