40a11b83c7abbc9d688677a6ee6cfc1b272ad348
tadasmartinkus <Tadas.Martinkus@consult.luminorgroup.com>
2019-09-14 09:09:13 +0300

    init project

---
+++ src/main/java/us/martink/uniquex/UniquexApplication.java
@@ -0,0 +1,11 @@
+package us.martink.uniquex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniquexApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UniquexApplication.class, args);
+    }
+}
